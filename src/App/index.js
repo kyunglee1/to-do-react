4e3ccfd92649b95d9c98a5497a0907939fbb71e2
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-01-28 19:51:57 -0800

    Fix relative path to App

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import TodoList from '../TodoList/index';
+import './index.css';
 
 const App = () => <TodoList />;
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './TodoList/index';
+import App from './App/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
